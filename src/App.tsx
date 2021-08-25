7434dd6f6bd2101a74c2012fda421d621aedde76
appare45 <64473501+appare45@users.noreply.github.com>
2021-08-26 00:41:21 +0900

    addded pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logout from './components/logout';
 import User from './components/user';
+import Offline from './pages/offline';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <div className="App">
         <ChakraProvider>
-          <User path="/">
-            <Logout />
-          </User>
+          <Switch>
+            <Route exact path="/">
+              <User path="/">
+                <Logout />
+              </User>
+            </Route>
+            <Route path="/offline">
+              <Offline />
+            </Route>
+          </Switch>
         </ChakraProvider>
       </div>
     </BrowserRouter>
