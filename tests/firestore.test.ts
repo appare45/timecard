483e6b89cdbcf60fcbbc77e2a7880bc003e941a6
appare <64473501+appare45@users.noreply.github.com>
2022-02-15 17:02:14 +0900

    set up sample test

--- tests/firestore.test.ts
+++ tests/firestore.test.ts
@@ -1,5 +1,26 @@
-import * as firebase from '@firebase/rules-unit-testing';
-import * as fs from 'fs';
+import {
+  initializeTestEnvironment,
+  RulesTestEnvironment,
+} from '@firebase/rules-unit-testing';
+import { readFileSync } from 'fs';
 
-const PROJECT_ID = 'rule-test';
-const RULES_PATH = 'firestore.rules';
+// test environment
+export let testEnv: RulesTestEnvironment;
+beforeAll(async () => {
+  // テスト環境の初期化
+  testEnv = await initializeTestEnvironment({
+    projectId: 'demo-clubroom-testing',
+    firestore: {
+      rules: readFileSync('firestore.rules', 'utf-8'),
+    },
+  });
+});
+
+afterEach(() => {
+  testEnv.cleanup();
+});
+
+// 動作チェック
+test('Check jet', () => {
+  expect(2 + 2).toBe(4);
+});
