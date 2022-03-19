55dd5bec5f2f578df62076116e085e4636673a02
appare <64473501+appare45@users.noreply.github.com>
2022-03-20 01:58:03 +0900

    fixed end all running activitities UI bug

---
+++ src/pages/EndAllActivity.tsx
@@ -0,0 +1,99 @@
+import { HStack, Spacer, VStack } from '@chakra-ui/layout';
+import { Alert, AlertIcon } from '@chakra-ui/alert';
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/modal';
+import { QueryDocumentSnapshot, Timestamp } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
+import { useDisclosure } from '@chakra-ui/hooks';
+import { IoCheckmarkDone } from 'react-icons/io5';
+import ActivityCard from '../components/activity-card';
+import { BasicButton, CancelButton } from '../components/buttons';
+import { GroupContext } from '../contexts/group';
+import { activity, getAllActivities, setWork, work } from '../utils/group';
+import { Progress } from '@chakra-ui/progress';
+
+export const EndAllActivity = (): JSX.Element => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { currentGroup } = useContext(GroupContext);
+  const [AllRunningActivities, setAllRunningActivities] = useState<
+    QueryDocumentSnapshot<activity<work>>[] | null
+  >(null);
+  useEffect(() => {
+    if (!!currentGroup && isOpen)
+      getAllActivities({ groupId: currentGroup.id, onlyRunning: true }).then(
+        (e) => {
+          setAllRunningActivities(e);
+        }
+      );
+  }, [currentGroup, isOpen]);
+  const [endingCount, setEndingCount] = useState(0);
+  return (
+    <>
+      <BasicButton
+        variant="secondary"
+        leftIcon={<IoCheckmarkDone />}
+        onClick={onOpen}
+      >
+        進行中のアクティビティーをすべて終了
+      </BasicButton>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>進行中のアクティビティーをすべて終了</ModalHeader>
+          <ModalBody>
+            {!!AllRunningActivities?.length && (
+              <>
+                <HStack>
+                  {AllRunningActivities.length}個の進行中のアクティビティー
+                  <Spacer />
+                  <CancelButton
+                    variant="primary"
+                    onClick={async () => {
+                      Promise.all(
+                        AllRunningActivities.map(async (e, i) => {
+                          const _runningActivity = e.data();
+                          _runningActivity.content.endTime = Timestamp.now();
+                          _runningActivity.content.status = 'done';
+                          _runningActivity.updated = Timestamp.now();
+                          if (currentGroup)
+                            await setWork(
+                              currentGroup.id,
+                              e.id,
+                              _runningActivity,
+                              {}
+                            );
+                          setEndingCount(i);
+                        })
+                      ).then(() => setAllRunningActivities([]));
+                    }}
+                  >
+                    終了
+                  </CancelButton>
+                </HStack>
+                <Progress
+                  value={(endingCount / AllRunningActivities.length) * 100}
+                  colorScheme="green"
+                />
+                <VStack>
+                  {AllRunningActivities?.map((e) => (
+                    <ActivityCard key={e.id} activitySnapshot={e} />
+                  ))}
+                </VStack>
+              </>
+            )}
+            {AllRunningActivities?.length === 0 && (
+              <Alert status="success">
+                <AlertIcon /> 進行中のアクティビティーが存在しません。
+              </Alert>
+            )}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -1,16 +1,6 @@
-import { HStack, Spacer, Text, VStack } from '@chakra-ui/layout';
-import { Alert, AlertIcon } from '@chakra-ui/alert';
-import {
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-} from '@chakra-ui/modal';
-import { QueryDocumentSnapshot, Timestamp } from 'firebase/firestore';
-import React, { Suspense, useContext, useEffect, useState } from 'react';
-import { useDisclosure } from '@chakra-ui/hooks';
-import { IoCheckmarkDone, IoScan } from 'react-icons/io5';
+import { Text } from '@chakra-ui/layout';
+import React, { Suspense, useContext, useState } from 'react';
+import { IoScan } from 'react-icons/io5';
 import {
   useRouteMatch,
   useHistory,
@@ -18,95 +8,13 @@ import {
   Route,
   Link as RouterLink,
 } from 'react-router-dom';
-import ActivityCard from '../components/activity-card';
 import { LoadingScreen } from '../components/assets';
-import { BasicButton, CancelButton } from '../components/buttons';
+import { BasicButton } from '../components/buttons';
 import { GroupContext } from '../contexts/group';
 import { GroupTemplate } from '../templates/group';
 import { dataWithId } from '../utils/firebase';
-import { activity, getAllActivities, setWork, work } from '../utils/group';
 import { Member } from '../utils/member';
-import { Progress } from '@chakra-ui/react';
-
-const EndAllActivity = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { currentGroup } = useContext(GroupContext);
-  const [AllRunningActivities, setAllRunningActivities] = useState<
-    QueryDocumentSnapshot<activity<work>>[] | null
-  >(null);
-  useEffect(() => {
-    if (!!currentGroup && isOpen)
-      getAllActivities({ groupId: currentGroup.id, onlyRunning: true }).then(
-        (e) => {
-          setAllRunningActivities(e);
-        }
-      );
-  }, [currentGroup, isOpen]);
-  const [endingCount, setEndingCount] = useState(0);
-  return (
-    <>
-      <BasicButton
-        variant="secondary"
-        leftIcon={<IoCheckmarkDone />}
-        onClick={onOpen}
-      >
-        進行中のアクティビティーをすべて終了
-      </BasicButton>
-      <Modal isOpen={isOpen} onClose={onClose} size="xl">
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>進行中のアクティビティーをすべて終了</ModalHeader>
-          <ModalBody>
-            {AllRunningActivities?.length && (
-              <>
-                <HStack>
-                  {AllRunningActivities.length}個の進行中のアクティビティー
-                  <Spacer />
-                  <CancelButton
-                    variant="primary"
-                    onClick={async () => {
-                      Promise.all(
-                        AllRunningActivities.map(async (e, i) => {
-                          const _runningActivity = e.data();
-                          _runningActivity.content.endTime = Timestamp.now();
-                          _runningActivity.content.status = 'done';
-                          _runningActivity.updated = Timestamp.now();
-                          if (currentGroup)
-                            await setWork(
-                              currentGroup.id,
-                              e.id,
-                              _runningActivity,
-                              {}
-                            );
-                          setEndingCount(i);
-                        })
-                      ).then(() => setAllRunningActivities([]));
-                    }}
-                  >
-                    終了
-                  </CancelButton>
-                </HStack>
-                <Progress
-                  value={(endingCount / AllRunningActivities.length) * 100}
-                />
-                <VStack>
-                  {AllRunningActivities?.map((e) => (
-                    <ActivityCard key={e.id} activitySnapshot={e} />
-                  ))}
-                </VStack>
-              </>
-            )}
-            {AllRunningActivities?.length === 0 && (
-              <Alert status="success">
-                <AlertIcon /> 進行中のアクティビティーが存在しません。
-              </Alert>
-            )}
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
-  );
-};
+import { EndAllActivity } from './EndAllActivity';
 
 const Timeline: React.FC = () => {
   const { path } = useRouteMatch();
