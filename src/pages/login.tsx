f50abcf5a8356b65881ada7c29dc5bca8fa47905
appare <64473501+appare45@users.noreply.github.com>
2022-10-22 11:25:23 +0900

    Added Firebase UI for login button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,42 +1,21 @@
 import React from 'react';
-import { Box, Center, Heading, Text } from '@chakra-ui/layout';
+import { Box, Center, Heading } from '@chakra-ui/layout';
 import { Image } from '@chakra-ui/image';
-import { IoLogoGoogle } from 'react-icons/io5';
-import {
-  GoogleAuthProvider,
-  signInWithRedirect,
-  useDeviceLanguage,
-} from 'firebase/auth';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+import { GoogleAuthProvider } from 'firebase/auth';
 import { useUniversalColors } from '../hooks/color-mode';
 import { auth } from '../utils/auth';
-import { Button } from '@chakra-ui/button';
 
 const FirebaseAuth: React.FC<{ redirectUri: string; isLoading: boolean }> = ({
   redirectUri,
-  isLoading,
 }) => {
-  const provider = new GoogleAuthProvider();
-  useDeviceLanguage(auth());
-  provider.setCustomParameters({
-    redirect_uri: redirectUri,
-  });
-  return (
-    <Button
-      w={'full'}
-      maxW={'md'}
-      variant={'outline'}
-      onClick={() => signInWithRedirect(auth(), provider)}
-      leftIcon={<IoLogoGoogle />}
-      isLoading={isLoading}
-      colorScheme="gray"
-    >
-      <Center>
-        <Text>Sign in with Google</Text>
-      </Center>
-    </Button>
-  );
+  const loginUiConfig: firebaseui.auth.Config = {
+    signInFlow: 'redirect',
+    signInSuccessUrl: redirectUri,
+    signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+  };
+  return <StyledFirebaseAuth uiConfig={loginUiConfig} firebaseAuth={auth()} />;
 };
-
 export default function Login(props: {
   redirectUri: string;
   isLoading: boolean;
